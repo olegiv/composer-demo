e81d6ff6880cc96625525b532162b65c4ab1a5a5
Oleg Ivanchenko <oleg.ivanchenko@iru.org>
2023-03-31 17:04:27 +0200

    PrintMessage class added.

---
+++ src/PrintMessage.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Olegiv\ComposerDemo;
+
+/**
+ *
+ */
+class PrintMessage {
+
+	/**
+	 * @return void
+	 */
+	public static function print(): void {
+
+		echo "\n" . 'The good one' . "\n";
+	}
+}
